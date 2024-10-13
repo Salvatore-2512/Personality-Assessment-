2140bddac5a81b8087c948c5644fdc963d882738
Aryan Vasudeva <aryanvasudeva@hotmail.com>
2024-10-13 12:54:38 +0300

    Added the project Files

---
+++ script.js
@@ -0,0 +1,438 @@
+const quizData = [
+    {
+        question: "I find it easy to take charge of a situation:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I can stare people out:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I often raise my voice to achieve results:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I find it easy to put forward my viewpoint even when other people are trying to put forward theirs:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I often wag my finger at people or jab in their direction to emphasise a point:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I believe that people should behave in certain acceptable ways:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I can be very persistent and usually obtain my own way:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "If anyone tries to 'bully' me, I can give as good as I receive:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I can use sarcasm to let people know what I think of them:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I rarely see the validity of people's criticism of me:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "If other people land themselves in difficulties, it is their problem:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I believe that being open about one's emotions is a sign of vulnerability, so I rarely do it:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+
+     //---------------
+
+    {
+        question: "I conform to accept behaviour even if it is not what I choose myself:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I would feel vulnerable if I was suddenly put in charge of handling a crisis:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I often wring my hands together when nervous:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+   
+    {
+        question: "I'm often critical of my own behaviour:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+
+    {
+        question: "I find it difficult to look people in the eye when there is a problem between us:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    
+    {
+        question: "The hesitancy in my voice makes me appear nervous:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I feel uncomfortable when people criticize me:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I would embarrass me to tell people how I really feel:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I find it easy to let people put forward their views in preference to my own:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I find it difficult to stand my ground in an argument with someone else:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I feel easily intimidated by more forceful people:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I find it easy to relate to, and feel responsible for, other people's problems:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+
+    //--------------------
+
+    {
+        question: "The volume of my voice, when faced with confrontation, is above average:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+
+    {
+        question: "I don't use sarcasm and ridicule to achieve results:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I believe that how people behave is up to them:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    
+    {
+        question: "Even if I do get nervous I rarely fidget:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "Even when listening to the views of other people, I keep sight of my own views:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "When people try and obtain their way by making me feel sympathetic toward them, I ensure they stay responsible for finding their own solution:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I believe that by being open about how I feel assists in achieving clear communication:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "When people try and force me to do something, I usually manage to achieve a compromise with which we are both happy:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    
+    {
+        question: "I tend to approach problems in a consultative and democratic manner:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    
+    
+    
+    {
+        question: "I have learned a lot by listening to criticism:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    
+    
+    
+    {
+        question: "I find that standing my ground usually encourages the other person to come to a compromise:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    },
+    {
+        question: "I will make my eye contact with people, but not to such an extent that they are made uncomfortable about it:",
+        options: [
+            { answer: "Totally Untrue", points: 0 },
+            { answer: "Occasionally True", points: 1 },
+            { answer: "Often True", points: 2 },
+            { answer: "Totally True", points: 3 }
+        ]
+    }
+];
+
+
+// Your existing quizContainer and createQuizSection code remains unchanged.
+
+const quizContainer = document.getElementById('quiz-container');
+
+const createQuizSection = (questions, title) => {
+    const sectionTitle = document.createElement('h2');
+    sectionTitle.innerText = title;
+    quizContainer.appendChild(sectionTitle);
+
+    questions.forEach((item, index) => {
+        const quizItem = document.createElement('div');
+        quizItem.classList.add('quiz-item');
+
+        const questionElement = document.createElement('h3');
+        questionElement.innerText = `${index + 1}. ${item.question}`;
+        quizItem.appendChild(questionElement);
+
+        const optionsContainer = document.createElement('div');
+        optionsContainer.classList.add('options');
+
+        item.options.forEach((option) => {
+            const optionButton = document.createElement('button');
+            optionButton.classList.add('option-btn');
+            optionButton.innerText = option.answer;
+
+            optionButton.addEventListener('click', () => {
+                // Clear previous selections
+                const selectedOptions = optionsContainer.querySelectorAll('.selected-option');
+                selectedOptions.forEach(btn => btn.classList.remove('selected-option'));
+
+                // Mark the selected option
+                optionButton.classList.add('selected-option');
+
+                // Save the selected points
+                optionButton.dataset.points = option.points;
+            });
+
+            optionsContainer.appendChild(optionButton);
+        });
+
+        quizItem.appendChild(optionsContainer);
+        quizContainer.appendChild(quizItem);
+    });
+};
+
+// Create sections for Aggressive, Submissive, and Assertive questions
+const aggressiveQuestions = quizData.slice(0, 12); // First 12 questions
+const submissiveQuestions = quizData.slice(12, 24); // Next 12 questions
+const assertiveQuestions = quizData.slice(24, 36); // Last 12 questions
+
+createQuizSection(aggressiveQuestions, "Aggressive Properties");
+createQuizSection(submissiveQuestions, "Submissive Properties");
+createQuizSection(assertiveQuestions, "Assertive Properties");
+
+// Submit button for scoring
+const submitButton = document.createElement('button');
+submitButton.innerText = 'Submit';
+submitButton.addEventListener('click', () => {
+    let aggressiveScore = 0;
+    let submissiveScore = 0;
+    let assertiveScore = 0;
+
+    // Calculate total scores
+    const aggressiveOptions = document.querySelectorAll('.quiz-item:nth-child(-n+12) .options');
+    aggressiveOptions.forEach(container => {
+        const selectedOption = container.querySelector('.selected-option');
+        if (selectedOption) {
+            aggressiveScore += parseInt(selectedOption.dataset.points, 10) || 0;
+        }
+    });
+
+    const submissiveOptions = document.querySelectorAll('.quiz-item:nth-child(n+13):nth-child(-n+24) .options');
+    submissiveOptions.forEach(container => {
+        const selectedOption = container.querySelector('.selected-option');
+        if (selectedOption) {
+            submissiveScore += parseInt(selectedOption.dataset.points, 10) || 0;
+        }
+    });
+
+    const assertiveOptions = document.querySelectorAll('.quiz-item:nth-child(n+25) .options');
+    assertiveOptions.forEach(container => {
+        const selectedOption = container.querySelector('.selected-option');
+        if (selectedOption) {
+            assertiveScore += parseInt(selectedOption.dataset.points, 10) || 0;
+        }
+    });
+
+    // Redirect to score page with scores as query parameters
+    window.location.href = `score.html?score1=${aggressiveScore}&score2=${submissiveScore}&score3=${assertiveScore}`;
+});
+
+quizContainer.appendChild(submitButton);
